3880faae2624a0bc85e07ede6430447b7a06fc1e
Anthony  Lemus <anthony.lemus@gmail.com>
2024-02-16 13:33:32 -0600

    2-16-2024

---
+++ least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/?envType=daily-question&envId=2024-02-16
+from collections import Counter
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        counts = Counter(arr).values()
+        
+        counts = sorted(counts, reverse = True)
+        n = len(counts)
+        while n>0 and k>=counts[-1]:
+            k-=counts.pop()
+            n-=1
+        return len(counts)
